59c40d83806590437be3393f6a8cde955100ac97
MuruganRaj <muruga4046@gmail.com>
2015-01-20 15:25:44 +0530

    JANA

--- src/com/example/sample/MainActivity.java
+++ src/com/example/sample/MainActivity.java
@@ -89,6 +89,31 @@ public class MainActivity extends ActionBarActivity {
 		}
 	
 	
+	public String PerfectDecimal7(String str, int MAX_BEFORE_POINT, int MAX_DECIMAL){
+		  if(str.charAt(0) == '.') str = "0"+str;
+		  int max = str.length();
+
+		  String rFinal = "";
+		  boolean after = false;
+		  int i = 0, up = 0, decimal = 0; char t;
+		  while(i < max){
+		    t = str.charAt(i);
+		    if(t != '.' && after == false){
+		        up++;
+		        if(up > MAX_BEFORE_POINT) return rFinal;
+		    }else if(t == '.'){
+		        after = true;
+		    }else{
+		        decimal++;
+		        if(decimal > MAX_DECIMAL)
+		            return rFinal;
+		    }
+		    rFinal = rFinal + t;
+		    i++;
+		  }return rFinal;
+		}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
